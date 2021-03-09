a9dad2d0b74f71bf4cf4a906899bfddcf2433513
Maroš Hajník <hajnik.maros@gmail.com>
2021-03-09 10:33:51 +0100

    Custom Router State Serializers

--- HajnikPortfolio/ClientApp/src/app/store/reducers/index.ts
+++ HajnikPortfolio/ClientApp/src/app/store/reducers/index.ts
@@ -1,4 +1,9 @@
-import { Params } from "@angular/router";
+import {
+  ActivatedRoute,
+  RouterStateSnapshot,
+  Params,
+  ActivatedRouteSnapshot,
+} from "@angular/router";
 import { createFeatureSelector, ActionReducerMap } from "@ngrx/store";
 
 import * as fromRouter from "@ngrx/router-store";
@@ -20,3 +25,20 @@ export const reducers: ActionReducerMap<State> = {
 export const getRouterState = createFeatureSelector<
   fromRouter.RouterReducerState<RouterStateUrl>
 >("routerReducer");
+
+export class CustomSerializer
+  implements fromRouter.RouterStateSerializer<RouterStateUrl> {
+  serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+    const { url } = routerState;
+    const { queryParams } = routerState.root;
+
+    let state: ActivatedRouteSnapshot = routerState.root;
+    while (state.firstChild) {
+      state = state.firstChild;
+    }
+
+    const { params } = state;
+
+    return { url, queryParams, params };
+  }
+}
